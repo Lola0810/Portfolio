3ceb08851953bea1ad0469a14b3a7059353ba792
Lola <navarro.lola@protonmail.com>
2024-11-17 14:43:38 +0100

    Projects modification

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -21,8 +21,9 @@ const PROJECTS = [
             "Logiciels: VSCode, Adobe XD", 
             "Langages de programmation: HTML CSS PHP"
         ],
+        softskill: "Leadership, Rigueur, Solidarité",
         note: [
-            "Premier projet lycéen avec du PHP - leadership, prise d’initiative, esprit d’équipe",
+            "Premier projet de lycée avec du PHP",
             "À voir sur : <a href='https://github.com/Lola0810/repertoire-web'>Ici Répertoire Github</a>"
             ]
     },
@@ -49,6 +50,7 @@ const PROJECTS = [
             "Logiciels: VSCode, Adobe XD, Webstorm", 
             "Langages de programmation: HTML CSS PHP JS NodeJS ReactJS"
         ],
+        softskill: "Polyvalence, auto-didacte, esprit d'équipe",
         note: [
             "Dernier projet de NSI - Découverte d'un framework Javascript",
             "À voir sur : <a href='https://github.com/Lola0810/repertoire-web'>Ici Répertoire Github</a>"
@@ -77,6 +79,7 @@ const PROJECTS = [
             "Langages de programmation: HTML CSS PHP",
             "Outils de gestion de projet: Google Agenda, GANTT, Notion"
         ],
+        softskill: "Rigueur, Prise d’initiative, Autonomie",
         note: [
             "Le but de ce projet était de réaliser un pokedex pokemon dans lequel selon le dresseur que l'on selectionnait on pouvait soit ajouter un pokémon, soit en retirer (selon la quantité) mais également le renommer et voir son détail.",
             "A voir sur : <a href='https://www.canva.com/design/DAF4v1audBw/pnIEMRp4sh6kSsLn5gXwcA/view?utm_content=DAF4v1audBw&utm_campaign=designshare&utm_medium=link&utm_source=editor'>Book avec plus d'images</a> et <a href='https://github.com/Lola0810/Pokedex_Pokemon'>Répertoire Github</a>"
@@ -105,6 +108,7 @@ const PROJECTS = [
             "Langages de programmation: HTML CSS JS",
             "Outils de gestion de projet: Notion, Google Agenda"
         ],
+        softskill: "Créativité, Organisation, Polyvalence",
         note: [
             "Premier site portfolio, celui-ci avait pour but de sensibiliser sur l'importance d'avoir un portfolio pour se vendre mais également d'avoir un visuel unique qui permet de se démarquer.",
             "À voir sur : <a href='https://www.canva.com/design/DAF4v1audBw/pnIEMRp4sh6kSsLn5gXwcA/view?utm_content=DAF4v1audBw&utm_campaign=designshare&utm_medium=link&utm_source=editor'>Book avec plus d'images</a> et <a href='https://github.com/Lola0810/First_Portfolio'>Répertoire Github</a>"
@@ -132,6 +136,7 @@ const PROJECTS = [
             "Logiciels: VSCode, Github, Figma, Wampp", 
             "Langages de programmation: HTML CSS PHP SQL"
         ],
+        softskill: "Organisation, Polyvalence, Esprit d’équipe",
         note: [
             "Création d'un site internet qui permet à un utilisateur de regarder son stock de confiseries, regarder les confiseries d'autres boutiques mais pas d'acheter. C'est le premier projet web universitaire en groupe, j'ai appris à mieux gérer un planning et l'importance de la communication en équipe.", 
             "À voir le code source sur : <a href='https://github.com/Lola0810/First_Portfolio'>Répertoire Github</a>"
@@ -159,6 +164,7 @@ const PROJECTS = [
             "Logiciels: Figma, Photoshop, Illustrator", 
             "Outils de gestion de projet: Notion, GANTT"
         ],
+        softskill: "Leadership, Créativité, Innovation",
         note: [
             "Création d'un design d'application web pour permettre aux utilisateurs de réserver dans les 5 salles du Trégor facilement via une application.",
             "À voir sur : <a href='https://www.canva.com/design/DAF4v1audBw/pnIEMRp4sh6kSsLn5gXwcA/view?utm_content=DAF4v1audBw&utm_campaign=designshare&utm_medium=link&utm_source=editor'>Book avec plus d'images</a>"
@@ -185,6 +191,7 @@ const PROJECTS = [
             "Logiciels: EXCEL, Canva, Figma", 
             "Outils: User Journey Map - QuickStoryBoard - BluePrint"
         ],
+        softskill: "Analyse, Conceptualisation, Synthèse",
         note: [
             "Analyse des usages et usagers de la bibliothèque universitaire, nous avons donc analysé les étapes pour différentes actions.",
             "À voir l'analyse dans mes projets Linkedin : <a href='https://www.linkedin.com/in/lola-navarro/details/projects/'>Projet sur LinkedIn</a> et dans <a href='https://www.canva.com/design/DAF4v1audBw/pnIEMRp4sh6kSsLn5gXwcA/view?utm_content=DAF4v1audBw&utm_campaign=designshare&utm_medium=link&utm_source=editor'>mon book</a>."
@@ -215,6 +222,7 @@ const PROJECTS = [
             "Langages de programmation: HTML, CSS, JavaScript, PHP, CodeIgniter",
             "Outils de gestion de projet: Notion, Google drive"
         ],
+        softskill: "Organisation, Réactivité aux problèmes, Persévérance",
         note: [
             "Création d'un site internet from Scratch à partir d'un persona définie. Dans notre cas nous avons eu le persona d'une personne âgée cherchant à louer du matériel sportif facilement car il n'aime pas internet.",
             "À voir sur : <a href='https://www.canva.com/design/DAF4v1audBw/pnIEMRp4sh6kSsLn5gXwcA/view?utm_content=DAF4v1audBw&utm_campaign=designshare&utm_medium=link&utm_source=editor'>Book avec plus d'images</a> et <a href='https://github.com/Lola0810/First_Portfolio'>Répertoire Github</a>"
@@ -244,6 +252,7 @@ const PROJECTS = [
             "Logiciels: Affinity Photo, Affinity Designer, Photoshop", 
             "Outils de gestion de projet: Notion, Gantt"
         ],
+        softskill: "Ecoute, Créativité, Synthèse",
         note: [
             "Création d'une affiche, d'un logo et d'un flyer en groupe pour l'évènement Faites Du Numérique de Plougrescant",
             "N'hésitez pas à regarder mon : <a href='https://www.canva.com/design/DAF4v1audBw/pnIEMRp4sh6kSsLn5gXwcA/view?utm_content=DAF4v1audBw&utm_campaign=designshare&utm_medium=link&utm_source=editor'>Book ici</a>"
@@ -271,6 +280,7 @@ const PROJECTS = [
         technologies: [
             "Logiciels: Affinity Designer, Photoshop, Illustrator, Procreate"
         ],
+        softskill: "Rigueur, Auto-apprentissage aux outils, Synthèse",
         note: [
             "Création d'une affiche en rapport avec l'un des sous-marins de l'Ifremer, dans mon cas j'ai choisi l'Ulyx. Par la suite mon affiche a été affichée dans le hall d'entrée de l'IUT de Lannion.",
             "N'hésitez pas à regarder mon : <a href='https://www.canva.com/design/DAF4v1audBw/pnIEMRp4sh6kSsLn5gXwcA/view?utm_content=DAF4v1audBw&utm_campaign=designshare&utm_medium=link&utm_source=editor'>Book ici</a>"
@@ -299,7 +309,7 @@ const PROJECTS = [
             "Logiciels: Photoshop, Procreate, Figma",
             "Outils de gestion de projet: Notion, Gantt, Google docs"
         ],
-        softskill: "Savoir, responsable, organisée",
+        softskill: "Créativité, Communication, Initiative",
         note: [
             "Nous avons été selectionné sur 13 projets pour la carte de voeux de l'IUT de Lannion adressée aux partenaires, aux élèves et à l'équipe pédagogique. Ce projet a été très enrichissant et nous a permis de comprendre l'importance d'avoir un univers graphique défini afin de réussir au mieux une affiche.",
             "N'hésitez pas à regarder mon : <a href='https://www.canva.com/design/DAF4v1audBw/pnIEMRp4sh6kSsLn5gXwcA/view?utm_content=DAF4v1audBw&utm_campaign=designshare&utm_medium=link&utm_source=editor'>Book</a>",
@@ -329,9 +339,39 @@ const PROJECTS = [
             "Logiciels: Figma, Google Docs, Photoshop, Affinity", 
             "Langues : Anglais et Français"
         ],
+        softskill: "Analyse, Communication, Compétences linguistiques",
         note: [
             "Création d'une affiche sur un produit afin de le promouvoir et de créer un design innovant et rafraichissant pour la marque",
             "N'hésitez pas à regarder mon : <a href='https://www.canva.com/design/DAF4v1audBw/pnIEMRp4sh6kSsLn5gXwcA/view?utm_content=DAF4v1audBw&utm_campaign=designshare&utm_medium=link&utm_source=editor'>Book</a>"
         ] 
+    },
+    
+    {   
+        type: "UX-UI",
+        class: "projects",
+        title: "ZenMail - Méthode ZéroInbox",
+        description: "Design d'expérience - 2024",
+        thumbnail: "zenmail.webp",
+        duration: {
+            start: "Septembre 2023",
+            end: "Octobre 2023"
+        },
+        baseline: "Projet mmi",
+        assignments: [
+            "• Etude de l’existant",
+            "• Etudes documentaires : opportunités d'amélioration",
+            "• Problématique sur l'opportunité choisie",
+            "• Scenario d'usage sur la solution créée",
+            "• Mockup"
+        ],
+        technologies: [
+            "Logiciels: Figma, Gmail, Excel", 
+            "Langues : Anglais et Français"
+        ],
+        softskill: "Entreprendre, Curiosité, Recherches",
+        note: [
+            "Création d'un design d'application web pour permettre aux utilisateurs de réserver dans les 5 salles du Trégor facilement via une application.",
+            "À voir sur : <a href='https://www.canva.com/design/DAF4v1audBw/pnIEMRp4sh6kSsLn5gXwcA/view?utm_content=DAF4v1audBw&utm_campaign=designshare&utm_medium=link&utm_source=editor'>Book avec plus d'images</a>"
+        ]
     },
 ];
